a946a8d796cf3fe126bb1c685e6b4c1be1fa4533
Yura <yorikyorik25@gmail.com>
2021-04-22 16:44:46 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/647px-Apple_logo_black.svg.png"></img>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Pokai_Bucht_auf_Oahu.jpg/1200px-Pokai_Bucht_auf_Oahu.jpg"></img>
+      </div>
+
+      <div>ava</div>
+
+      <div>
+        my post
+        <div>new post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
