d27dcf45311193789ade05e869c9dafbc3139c7f
Yura <yorikyorik25@gmail.com>
2021-04-23 17:18:47 +0300

    21 dialog verstka

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,33 @@ import d from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={d.dialogs}>
+            <div className={d.dialogsItem}>
+                <div className={d.dialog + ' ' + d.active}>
+                    Dimych
+                </div>
+                <div className={d.dialog}>
+                    Andrey
+                </div>
+                <div className={d.dialog}>
+                    Sveta
+                </div>
+                <div className={d.dialog}>
+                    Sasha
+                </div>
+                <div className={d.dialog}>
+                    Viktor
+                </div>
+                <div className={d.dialog}>
+                    Valera
+                </div>
+            </div>
+
+            <div className={d.messages}>
+                <div className={d.dialog}>Hi</div>
+                <div className={d.dialog}>How is your</div>
+                <div className={d.dialog}>Yo</div>
+            </div>
         </div>
     )
 }
