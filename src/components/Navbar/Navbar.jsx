d8573fc9491c94ec89f39a661d7ac234a77b2969
Yura <yorikyorik25@gmail.com>
2021-04-23 16:23:50 +0300

    19 route HomeWork

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,13 +11,13 @@ const Navbar = () => {
         <a href="/dialogs">Messages</a>
       </div>
       <div className={n.item}>
-        <a>News</a>
+        <a href="/news">News</a>
       </div>
       <div className={n.item}>
-        <a>Music</a>
+        <a href="/music">Music</a>
       </div>
       <div className={n.item}>
-        <a>Settings</a>
+        <a href="/settings">Settings</a>
       </div>
     </nav>
   );
