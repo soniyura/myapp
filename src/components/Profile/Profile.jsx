567c36bc533eea50b635c6e0c862a59ced5bf33c
Yura <yorikyorik25@gmail.com>
2021-04-23 13:23:48 +0300

    19 Dialogs

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import p from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className={p.content}>
+    <div>
       <div>
         <img src="https://fs.tonkosti.ru/sized/c960x400/3q/3r/3q3r3fgg4544os4c440gkskgg.jpg"></img>
       </div>
